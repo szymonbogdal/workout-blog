5973e8e8968e0f4135f8d1a8ca7aad3f2f143a7a
Szymon Bogdał <bogdal.symon@gmail.com>
2024-10-01 23:09:53 +0200

    database.php create users table

--- src/config/databse.php
+++ src/config/databse.php
@@ -14,13 +14,24 @@ class Database{
     }
     $this->createDatabase();
     $this->conn->select_db($this->db);
+
+    $this->createUsersTable();
   }
 
   private function createDatabase(){
     $sql = "CREATE DATABASE IF NOT EXISTS $this->db";
     $this->executeQuery($sql);
   }
-  
+
+  private function createUsersTable(){
+    $sql = "CREATE TABLE IF NOT EXISTS users(
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      username VARCHAR(50) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL
+    )";
+    $this->executeQuery($sql);
+  }
+
   public static function getInstance(){
     if(self::$instance === null){
       self::$instance = new Database();
