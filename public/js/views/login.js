71b599b819600b25ce592b097b3fcc42bab2c28d
Szymon Bogdał <bogdal.symon@gmail.com>
2024-10-04 21:36:22 +0200

    add login.js script to allow api call on form submit and to switch between login and register forms

---
+++ public/js/views/login.js
@@ -0,0 +1,33 @@
+import apiCall from "../apiCall.js";
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const form = document.getElementById("authForm");
+  const title = document.getElementById("formTitle");
+  const swapMsg = document.getElementById("swapMessage");
+  const swapAction = document.getElementById("swapAction");
+  const submitBtn = document.getElementById("submitButton");
+  const responseMsg = document.getElementById("responseMessage");
+
+  let isLogin = true;
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const url = `http://localhost/workout_blog/api/${isLogin ? "login" : "register"}`;
+    const result = await apiCall(url, "POST", formData);
+    if(result.status == 'success'){
+      responseMsg.innerHTML = "Your account has been created. You can log in now."
+    }else{
+      responseMsg.innerHTML = "Something wen wrong. Try again later";
+    }
+  });
+
+  swapAction.addEventListener('click', () => {
+    title.innerHTML = isLogin ? "Register" : "Login";
+    swapMsg.innerHTML = isLogin ? "Don't have account?" : "Already have account?";
+    swapAction.innerHTML = isLogin ? "Create account" : "Log in";
+    submitBtn.innerHTML = isLogin ? "Register" : "Login";
+    responseMsg.innerHTML = "";
+    isLogin = !isLogin;
+  })
+})
--- src/views/login.php
+++ src/views/login.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <script type="module" src="/workout_blog/public/js/views/login.js"></script>
 </head>
 <body>
   <h1 id="formTitle">Log in</h1>
