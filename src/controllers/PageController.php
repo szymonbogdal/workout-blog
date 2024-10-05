1b51c710129ca6f56c0392e2dd675462fe548de5
Szymon Bogdał <bogdal.symon@gmail.com>
2024-10-05 14:39:08 +0200

    PageController check if user is logged before rendering home or login view

--- src/controllers/PageController.php
+++ src/controllers/PageController.php
@@ -1,9 +1,15 @@
 <?php
 class PageController{  
   public function home(){
+    if(!isset($_SESSION['user_id']) || !isset($_SESSION['username'])){
+      header("Location: /workout_blog/login");
+    }
     require __DIR__ . "/../views/home.php";
   }
   public function login(){
+    if(isset($_SESSION['user_id']) && isset($_SESSION['username'])){
+      header("Location: /workout_blog");
+    }
     require __DIR__ . "/../views/login.php";
   }
 }
