4fe30849b1919687b8462987183e130dc37665ad
Szymon Bogdał <bogdal.symon@gmail.com>
2024-10-17 20:02:11 +0200

    home.php remove html structure that is now dynamically generated

--- src/views/home.php
+++ src/views/home.php
@@ -27,51 +27,8 @@
     </section>
   </div>
 
-  <div class="body">
-    <div class="workout">
-      <div class="workout__header">
-        <div>
-          <h2 class="header__tilte">Push Pull Legs Training</h2>
-          <div class="header__description">
-            <p class="header__description-item">by Szymon</p>
-            <p class="header__description-item">&#x2022;</p>
-            <p class="header__description-item">Intermediate level</p>
-          </div>
-        </div>
-        <div class="header__likes">
-          <button class="header__likes-button">&#x2764;</button>
-          <p class="header__likes-count">123</p>
-        </div>  
-      </div>
-      <div class="workout__container">
-        <div class="workout__day">
-          <h4 class="workout__day-title">Push day</h4>
-          <ul class="workout__day-list">
-            <li class="workout__day-exercise">Flat Bench press</li>
-            <li class="workout__day-exercise">Incline Bench press</li>
-            <li class="workout__day-exercise">Rope pulldown</li>
-            <li class="workout__day-exercise">Lateral rises</li>
-          </ul>
-        </div>
-        <div class="workout__day">
-          <h4 class="workout__day-title">Pull day</h4>
-          <ul>
-            <li class="workout__day-exercise">Lat pulldown</li>
-            <li class="workout__day-exercise">T-bar row</li>
-            <li class="workout__day-exercise">Biceps curls</li>
-          </ul>
-        </div>
-        <div class="workout__day">
-          <h4 class="workout__day-title">Leg day</h4>
-          <ul>
-            <li class="workout__day-exercise">Squat</li>
-            <li class="workout__day-exercise">Lep extensions</li>
-            <li class="workout__day-exercise">Leg curls</li>
-            <li class="workout__day-exercise">Calf raises</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+  <div class="workout-container" id="workoutContainer">
+    
   </div>
 
   <div class="modal" id="modal">
