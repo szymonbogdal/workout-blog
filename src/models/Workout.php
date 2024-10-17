baa33fa7e5ef1dc042677d8356c607fb4e441624
Szymon Bogdał <bogdal.symon@gmail.com>
2024-10-17 19:59:28 +0200

    Workout.php getWorkouts method also returns working_days of each workout

--- src/models/Workout.php
+++ src/models/Workout.php
@@ -6,86 +6,117 @@ class Workout{
     $this->db = Database::getInstance()->getConnection();
   }
   public function getWorkouts($params){
-    //base query
-    $sql = 
-    "SELECT 
-      p.id,
-      p.title,
-      p.body,
-      p.week_days,
-      p.created_at,
-      p.updated_at,
-      u.username AS author,
-      (SELECT COUNT(*) FROM workout_likes wl WHERE wl.workout_id = p.id) AS like_count,
-      CASE 
-        WHEN ? IS NOT NULL AND EXISTS (
-          SELECT 1 FROM workout_likes wl2 
-          WHERE wl2.workout_id = p.id AND wl2.user_id = ?
-        ) THEN 1 
-        ELSE 0 
-        END AS is_liked_by_user,
-      CASE 
-        WHEN ? IS NOT NULL AND p.user_id = ? THEN 1 
-        ELSE 0 
-      END AS is_user_author
-    FROM workouts p
-    JOIN users u ON p.user_id = u.id
-    ";
+    try{
+      //base query
+      $workoutSql = 
+      "SELECT 
+        w.id,
+        w.title,
+        w.difficulty,
+        w.created_at,
+        w.updated_at,
+        u.username AS author,
+        (SELECT COUNT(*) FROM workout_days wd WHERE wd.workout_id = w.id) AS week_days,
+        (SELECT COUNT(*) FROM workout_likes wl WHERE wl.workout_id = w.id) AS like_count,
+        CASE 
+          WHEN ? IS NOT NULL AND EXISTS (
+            SELECT 1 FROM workout_likes wl2 
+            WHERE wl2.workout_id = w.id AND wl2.user_id = ?
+          ) THEN 1 
+          ELSE 0 
+          END AS is_liked_by_user
+      FROM workouts w
+      JOIN users u ON w.user_id = u.id
+      ";
 
-    //variables to hold additional data
-    //at start assign user to null
-    $conditions = [];
-    $values = [null,null,null,null];
-    $types = "iiii";
+      //variables to hold additional data
+      //if there is usser assign him to values
+      $workoutConditions = [];
+      $workoutValues = array_fill(0,2,$params['user_id'] ?? null);
+      $workoutTypes = "ii";
 
-    //if there is user provided, use his id in query
-    if(isset($params['user_id'])){
-      $values[0] = $params['user_id'];
-      $values[1] = $params['user_id'];
-      $values[2] = $params['user_id'];
-      $values[3] = $params['user_id'];
-    }
-
-    //Add filter parameters to query
-    if(isset($params['title'])){
-      $conditions[] = "p.title LIKE ?";
-      $values[] = "%".$params['title']."%";
-      $types .= "s";
-    }
-    if(isset($params['week_days'])){
-      $conditions[] = "p.week_days = ?";
-      $values[] = $params['week_days'];
-      $types .= "i";
-    }
-    if(!empty($conditions)){
-      $sql .= " WHERE ".implode(" AND ",$conditions);
-    }
-
-    //Allow ordering by some fields
-    $sortFields = ["like_count", "created_at", "updated_at", "week_days"];
-    $sortOrders = ["ASC", "DESC"];
-    if(isset($params['sort']) && in_array(strtolower($params['sort']), $sortFields)){
-      $sql .= " ORDER BY ".strtolower($params['sort']);
-      if(isset($params['order']) && in_array(strtoupper($params['order']), $sortOrders)){
-        $sql .= " ".strtoupper($params['order']);
+      //Add filter parameters to query
+      if(isset($params['title'])){
+        $workoutConditions[] = "w.title LIKE ?";
+        $workoutValues[] = "%".$params['title']."%";
+        $workoutTypes .= "s";
       }
-    }else{
-      $sql .= " ORDER BY like_count DESC";
+
+      if(isset($params['difficulty'])){
+        $workoutConditions[] = "w.difficulty LIKE ?";
+        $workoutValues[] = "%".$params['difficulty']."%";
+        $workoutTypes .= "s";
+      }
+
+      if(!empty($workoutConditions)){
+        $workoutSql .= " WHERE ".implode(" AND ",$workoutConditions);
+      }
+
+      if(isset($params['week_days'])){
+        $workoutSql .= " HAVING week_days = ?";
+        $workoutValues[] = $params["week_days"];
+        $workoutTypes .= "i";
+      }
+
+      //Allow ordering by some fields
+      $sortFields = ["like_count", "created_at", "updated_at", "week_days"];
+      $sortOrders = ["ASC", "DESC"];
+      if(isset($params['sort']) && in_array(strtolower($params['sort']), $sortFields)){
+        $workoutSql .= " ORDER BY ".strtolower($params['sort']);
+        if(isset($params['order']) && in_array(strtoupper($params['order']), $sortOrders)){
+          $workoutSql .= " ".strtoupper($params['order']);
+        }
+      }else{
+        $workoutSql .= " ORDER BY like_count DESC";
+      }
+      
+      //Prepare and execute query
+      $workoutStmt = $this->db->prepare($workoutSql);
+      if(!empty($workoutValues)){
+        $workoutStmt->bind_param($workoutTypes,...$workoutValues);
+      }
+      $workoutStmt->execute();
+      $workoutResult = $workoutStmt->get_result();
+      $workouts = [];
+      $workoutIds = [];
+      while($row =  $workoutResult->fetch_assoc()){
+        $workouts[] = $row;
+        $workouts[count($workouts)-1]['workout_days'] = [];
+        $workoutIds[] = $row['id'];
+      }
+
+      //Get workout_days assigned to returned workouts
+      if(!empty($workoutIds)){
+        $dayValues = implode(',', array_fill(0, count($workoutIds), "?"));
+        $daySql = 
+          "SELECT
+            workout_id,
+            day_order,
+            body
+            FROM workout_days
+            WHERE workout_id IN ($dayValues)
+            ORDER BY workout_id ASC, day_order ASC
+          ";
+        $dayStmt = $this->db->prepare($daySql);
+        $dayTypes = str_repeat('i', count($workoutIds));
+        $dayStmt->bind_param($dayTypes, ...$workoutIds);
+        $dayStmt->execute();
+        
+        //Assign workout_days to corresponding workouts
+        $workoutIndex = array_flip($workoutIds);  
+        $dayResult = $dayStmt->get_result();
+        while($row = $dayResult->fetch_assoc()){
+          $index = $workoutIndex[$row['workout_id']];
+          $workouts[$index]['workout_days'][] = [
+            "day_order" => $row['day_order'],
+            "body" => $row['body']
+          ];
+        }
+      }
+      return $workouts;
+    }catch(mysqli_sql_exception $e){
+      return ["status"=>"failed", "message"=>$e->getMessage()];
     }
-    
-    //Prepare and execute query, return data
-    $stmt = $this->db->prepare($sql);
-    if(!empty($values)){
-      $stmt->bind_param($types,...$values);
-    }
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $workouts = [];
-    while($row = $result->fetch_assoc()){
-      $workouts[] = $row;
-    }
-    $stmt->close();
-    return $workouts;
   }
 
   public function newWorkout($user_id, $title, $difficulty, $days){
