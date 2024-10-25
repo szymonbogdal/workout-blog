3109684bb3d9b37ad1c4b0b4d413e52574c6de41
Szymon Bogdał <bogdal.symon@gmail.com>
2024-10-25 13:31:20 +0200

    WorkoutCotntroller valdiate data before calling toogleLike method

--- src/controllers/WorkoutController.php
+++ src/controllers/WorkoutController.php
@@ -1,9 +1,11 @@
 <?php
 require_once __DIR__."/../models/Workout.php";
+require_once __DIR__ . "/../models/WorkoutLike.php";
 class WorkoutController{
-  private $workout;
+  private $workout, $workoutLike;
   public function __construct(){
     $this->workout = new Workout();
+    $this->workoutLike = new WorkoutLike();
   }
   public function workouts(){
     $params = $_GET;
@@ -29,5 +31,17 @@ class WorkoutController{
     }
     
     return $this->workout->newWorkout($_SESSION['user_id'], $params['title'], $params['difficulty'], $params['workoutDays']);
+  }
+
+  public function toggleLike(){
+    $params = json_decode(file_get_contents("php://input"), true);
+    if(!isset($_SESSION['user_id']) || !isset($_SESSION['username'])){
+      return ['status'=>"error", 'message'=>"You need to be logged to like workout."];
+    }
+    if(!isset($params['workout_id'])){
+      return ['status'=>"error", 'message'=>'Missing required parameters.'];
+    }
+    
+    return $this->workoutLike->toggleLike($params['workout_id'], $_SESSION['user_id']);
   }
 }
