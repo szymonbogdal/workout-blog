5fe186712a58c588693903659f2dbcb68e82265d
Szymon Bogdał <bogdal.symon@gmail.com>
2024-11-05 23:35:09 +0100

    web.php require UserController and user/statistics route

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,15 +2,19 @@
 require_once __DIR__ . '/../controllers/PageController.php';
 require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../controllers/WorkoutController.php';
+require_once __DIR__ . '/../controllers/UserController.php';
 class Router{
   private $routes = [ 
     "/api/register" => ["controller" => "AuthController", "method" => "register", 'type'=>'api'], 
     "/api/login" => ["controller" => "AuthController", "method" => "login", 'type'=>'api'], 
     "/api/logout" => ["controller" => "AuthController", "method" => "logout", 'type'=>'api'], 
+
     "/api/workouts" => ["controller" => "WorkoutController", "method" => "workouts", 'type'=>'api'], 
     "/api/workouts/new" => ["controller" => "WorkoutController", "method" => "newWorkout", 'type'=>'api'], 
     "/api/workouts/like" => ["controller" => "WorkoutController", "method" => "toggleLike", 'type'=>'api'], 
     "/api/workouts/delete" => ["controller" => "WorkoutController", "method" => "deleteWorkout", 'type'=>'api'], 
+
+    "/api/users/statistics" => ["controller" => "UserController", "method" => "getUserStatistics", 'type'=>'api'], 
 
     "/" => ["controller" => "PageController", "method" => "home", 'type'=>'page'],
     "/login" => ["controller" => "PageController", "method" => "login", 'type'=>'page'],
