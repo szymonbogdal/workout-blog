fc4a36586cbce807b628d57dbefa1e360dffc98d
Szymon Bogdał <bogdal.symon@gmail.com>
2024-11-06 15:26:17 +0100

    layout.php add defer to script tags

--- src/views/layout.php
+++ src/views/layout.php
@@ -7,10 +7,10 @@
   <link rel="stylesheet" href="/workout_blog/public/css/main.css">
   <?php
     if(isset($css)){
-      echo "<link rel='stylesheet' href='/workout_blog/public/css/views/$css'>";
+      echo "<link rel='stylesheet' href='/workout_blog/public/css/views/$css' defer>";
     }
     if(isset($js)){
-      echo "<script type='module' src='/workout_blog/public/js/views/$js'></script>";
+      echo "<script type='module' src='/workout_blog/public/js/views/$js' defer></script>";
     }
   ?>
   <script>
