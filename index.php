51918da28bc7e557e698e880d308ebd8ed1ce432
Szymon Bogdał <bogdal.symon@gmail.com>
2024-11-15 20:58:18 +0100

    index.php pass request method to router

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ session_start();
 require_once __DIR__ . "/src/routes/web.php";
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 $basePath = '/workout_blog';
-if (strpos($requestUri, $basePath) === 0) {
-    $requestUri = substr($requestUri, strlen($basePath));
+if(strpos($requestUri, $basePath) === 0){
+  $requestUri = substr($requestUri, strlen($basePath));
 }
 if($requestUri){
   $router = new Router();
-  $router->dispatch($requestUri);
+  $router->dispatch($requestUri, $requestMethod);
 }
