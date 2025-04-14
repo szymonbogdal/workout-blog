1eccec7887517daf98f7496373f936526991bee3
Szymon Bogdał <bogdal.symon@gmail.com>
2025-04-14 22:32:29 +0200

    update database import

--- src/models/User.php
+++ src/models/User.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/databse.php';
+require_once __DIR__ . '/../database/Database.php';
 class User{
   private $db;
   public function __construct(){
--- src/models/Workout.php
+++ src/models/Workout.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/databse.php';
+require_once __DIR__ . '/../database/Database.php';
 class Workout{
   private $db;
   public function __construct(){
